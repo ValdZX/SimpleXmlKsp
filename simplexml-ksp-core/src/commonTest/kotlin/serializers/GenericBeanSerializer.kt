deef9111e36d080cad4ff97d48bd78bf0c1213cd
vald_zx <vald_zx@csltd.com.ua>
2021-08-22 00:08:48 +0300

    SubGenerics concept

--- simplexml-ksp-core/src/commonTest/kotlin/serializers/GenericBeanSerializer.kt
+++ simplexml-ksp-core/src/commonTest/kotlin/serializers/GenericBeanSerializer.kt
@@ -29,36 +29,46 @@ object GenericBeanSerializer : ObjectSerializer<GenericBean<*, *>>() {
         obj: GenericBean<*, *>,
         genericTypeList: List<KTypeProjection>
     ) {
-        val t1Serializer = GlobalSerializersLibrary.findSerializers(genericTypeList[0].type?.classifier as KClass<Any>)
-        val t2Serializer = GlobalSerializersLibrary.findSerializers(genericTypeList[1].type?.classifier as KClass<Any>)
+        val typeArg1 = genericTypeList[0].type
+        val arguments1 = typeArg1?.arguments.orEmpty()
+        val typeArg2 = genericTypeList[1].type
+        val arguments2 = typeArg2?.arguments.orEmpty()
+        val t1Serializer = GlobalSerializersLibrary.findSerializers(typeArg1?.classifier as KClass<Any>)
+        val t2Serializer = GlobalSerializersLibrary.findSerializers(typeArg2?.classifier as KClass<Any>)
         tagFather.apply {
             val somObject1 = obj.somObject1 ?: throw SerializeException(
                 """field somObject1 value is
           required"""
             )
-            t1Serializer.buildXml(this, "somObject1", somObject1)
+            t1Serializer.buildXml(this, "somObject1", somObject1, arguments1)
             val somObject2 = obj.somObject2 ?: throw SerializeException(
                 """field somObject2 value is
           required"""
             )
-            t2Serializer.buildXml(this, "somObject2", somObject2)
+            t2Serializer.buildXml(this, "somObject2", somObject2, arguments2)
         }
     }
 
     override fun readData(element: XmlElement?, genericTypeList: List<KTypeProjection>):
             GenericBean<Any, Any> {
-        val t1Serializer = GlobalSerializersLibrary.findSerializers(genericTypeList[0].type?.classifier as KClass<*>)
-        val t2Serializer = GlobalSerializersLibrary.findSerializers(genericTypeList[1].type?.classifier as KClass<*>)
+        val typeArg1 = genericTypeList[0].type
+        val arguments1 = typeArg1?.arguments.orEmpty()
+        val typeArg2 = genericTypeList[1].type
+        val arguments2 = typeArg2?.arguments.orEmpty()
+        val t1Serializer = GlobalSerializersLibrary.findSerializers(typeArg1?.classifier as KClass<*>)
+        val t2Serializer = GlobalSerializersLibrary.findSerializers(typeArg2?.classifier as KClass<*>)
         element as TagXmlElement?
         val layer0Tag0 = element?.get("somObject1")
         val layer0Tag1 = element?.get("somObject2")
         return GenericBean<Any, Any>(
         ).apply {
             somObject1 = t1Serializer.readData(
-                layer0Tag0 ?: throw DeserializeException("""field somObject1 value is required""")
+                layer0Tag0 ?: throw DeserializeException("""field somObject1 value is required"""),
+                arguments1
             )
             somObject2 = t2Serializer.readData(
-                layer0Tag1 ?: throw DeserializeException("""field somObject2 value is required""")
+                layer0Tag1 ?: throw DeserializeException("""field somObject2 value is required"""),
+                arguments2
             )
         }
     }
--- simplexml-ksp-core/src/commonTest/kotlin/serializers/HandMadeSerializerTest.kt
+++ simplexml-ksp-core/src/commonTest/kotlin/serializers/HandMadeSerializerTest.kt
@@ -1,5 +1,6 @@
 package serializers
 
+import ua.vald_zx.simplexml.ksp.Element
 import ua.vald_zx.simplexml.ksp.GlobalSerializersLibrary
 import ua.vald_zx.simplexml.ksp.SimpleXml
 import kotlin.test.Test
@@ -18,6 +19,25 @@ class HandMadeSerializerTest {
         println(xml)
         val deserializedBean: GenericBean<String, Float> = SimpleXml.deserialize(xml)
         assertEquals(bean.somObject1, deserializedBean.somObject1)
+        assertEquals(bean.somObject2, deserializedBean.somObject2)
+    }
+
+    @Test
+    fun `GenericBeanSerializer with object test`() {
+        GlobalSerializersLibrary.add(GenericBean::class) {
+            GenericBeanSerializer
+        }
+        val bean = GenericBean<GenericBean<Double, Short>, Float>()
+        val genericBean = GenericBean<Double, Short>()
+        genericBean.somObject1 = 50.0
+        genericBean.somObject2 = 9
+        bean.somObject1 = genericBean
+        bean.somObject2 = 27f
+        val xml = SimpleXml.serialize(bean)
+        println(xml)
+        val deserializedBean: GenericBean<GenericBean<Double, Short>, Float> = SimpleXml.deserialize(xml)
+        assertEquals(bean.somObject1?.somObject1, deserializedBean.somObject1?.somObject1)
+        assertEquals(bean.somObject1?.somObject2, deserializedBean.somObject1?.somObject2)
         assertEquals(bean.somObject2, deserializedBean.somObject2)
     }
 }
