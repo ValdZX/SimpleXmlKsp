cb2a01cac19d8ea10cf0ec9e3e3b51c82cd3e11f
ValdZX <vald_zx@csltd.com.ua>
2021-10-03 15:22:14 +0300

    Kotlin 1.5.30 -> 1.5.31

--- buildSrc/src/main/kotlin/Configuration.kt
+++ buildSrc/src/main/kotlin/Configuration.kt
@@ -1,5 +1,5 @@
 const val projectGroup =  "io.github.valdzx"
 const val projectVersion = "1.0.0-dev08"
 
-const val kotlinVersion = "1.5.30"
-const val kspVersion = "1.5.30-1.0.0"
+const val kotlinVersion = "1.5.31"
+const val kspVersion = "1.5.31-1.0.0"
