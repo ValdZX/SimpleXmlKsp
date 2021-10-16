611481d56763edc17117c1f5636fdba3229af076
ValdZX <vald_zx@csltd.com.ua>
2021-10-16 11:26:38 +0300

    1.0.0-dev08 -> 1.0.0-dev09

--- buildSrc/src/main/kotlin/Configuration.kt
+++ buildSrc/src/main/kotlin/Configuration.kt
@@ -1,5 +1,5 @@
 const val projectGroup =  "io.github.valdzx"
-const val projectVersion = "1.0.0-dev08"
+const val projectVersion = "1.0.0-dev09"
 
 const val kotlinVersion = "1.5.31"
 const val kspVersion = "1.5.31-1.0.0"
